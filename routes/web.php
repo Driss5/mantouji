1d0c37a5897bdc17ba21983209155dec7c94bc09
Driss Bakhtaoui <drissbakhtaoui3@gmail.com>
2025-09-15 20:12:14 +0200

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('pages.home');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/', function () {
+    return view('pages.home');
+});
+
+Route::get('/client', function () {
+    return view('pages.client');
+})->middleware(['auth', 'verified'])->name('client');
+
+Route::get('/jammiya', function () {
+    return view('pages.jammiya');
+})->middleware(['auth', 'verified'])->name('jammiya');
+
+require __DIR__.'/auth.php';
